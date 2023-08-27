cb0a20ecf34aef6be32d3112c0b5703a72536c64
Alwaa <74717334+Alwaa@users.noreply.github.com>
2023-08-28 01:47:07 +0200

    Adding corectr for system monitoring and reordering dnf instlls

--- fedora-apps.sh
+++ fedora-apps.sh
@@ -7,7 +7,8 @@ sudo dnf upgrade
 sudo dnf install grub-customizer
 
 # General
-sudo dnf install htop gh neovim neofetch
+sudo dnf install htop corectrl neofetch
+sudo dnf install gh neovim
 #rpmfusion
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf groupupdate core
